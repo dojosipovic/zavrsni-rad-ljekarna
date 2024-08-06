c3a7c08a1237e7bbb08f71b5184882bba5fbe50b
Dominik Josipović <dominik.josipovic@gmail.com>
2024-08-06 19:59:44 +0200

    Dodana abstraktna klasa Repository

---
+++ Software/DataAccessLayer/Repositories/Repository.cs
@@ -0,0 +1,54 @@
+﻿using Mysqlx.Crud;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public abstract class Repository<T> : IDisposable where T : class
+    {
+        protected DrugstoreModel Context { get; set; }
+        public DbSet<T> Entities { get; set; }
+
+        public Repository(DrugstoreModel context)
+        {
+            Context = context;
+            Entities = Context.Set<T>();
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            var query = from e in Entities select e;
+            return query;
+        }
+
+        public int SaveChanges()
+        {
+            return Context.SaveChanges();
+        }
+
+        public virtual int Add(T entity, bool saveChanges = true)
+        {
+            Entities.Add(entity);
+            return saveChanges ? SaveChanges() : 0;
+        }
+
+        public abstract int Update(T entity, bool saveChanges = true);
+
+        public virtual int Remove(T entity, bool saveChanges = true)
+        {
+            Entities.Attach(entity);
+            Entities.Remove(entity);
+
+            return saveChanges ? SaveChanges() : 0;
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
