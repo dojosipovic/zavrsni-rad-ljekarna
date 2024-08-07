fe3c3e33e8642cd3dc5c0be451a72a120671d999
Dominik Josipović <dominik.josipovic@gmail.com>
2024-08-07 22:36:20 +0200

    Dodan servis za artikle

---
+++ Software/BusinessLogicLayer/ArtiklServices.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Repositories;
+using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class ArtiklServices
+    {
+        public List<Artikl> GetItems()
+        {
+            using(var repo = new ArtiklRepository())
+            {
+                return repo.GetAll().ToList();
+            }
+        }
+    }
+}
